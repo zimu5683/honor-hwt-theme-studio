5537c9e502756dac31a3f292ae10ed20b9d3c00d
zimu5683 <2485890442@qq.com>
2026-08-05 19:48:54 +0800

    release: v0.1.6.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.zimu5683.hwttransfer"
         minSdk = 33
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.1.6.1"
+        versionCode = 10
+        versionName = "0.1.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
