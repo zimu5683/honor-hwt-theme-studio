16145578c9ab8c61b2202a4d4117adf8ddeeff1d
zimu5683 <2485890442@qq.com>
2026-08-16 15:00:51 +0800

    chore: 版本号升级至 0.2.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.zimu5683.hwttransfer"
         minSdk = 33
         targetSdk = 36
-        versionCode = 21
-        versionName = "0.2.4"
+        versionCode = 22
+        versionName = "0.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- hwtstudio/__init__.py
+++ hwtstudio/__init__.py
@@ -1,3 +1,3 @@
 """HWT Theme Studio core package."""
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
