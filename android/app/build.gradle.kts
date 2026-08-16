26c1828a898c7aa44c8d26b22d10d507af69f8f5
zimu5683 <2485890442@qq.com>
2026-08-16 08:42:36 +0800

    chore: 版本号升级至 0.2.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.zimu5683.hwttransfer"
         minSdk = 33
         targetSdk = 36
-        versionCode = 18
-        versionName = "0.2.1"
+        versionCode = 19
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
